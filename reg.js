c2661598d7a239a4559754bceba4b879073071fc
Patrick Oliveira <70863874+patrck-ak@users.noreply.github.com>
2021-06-27 23:05:59 -0300

    index-dash

--- reg.js
+++ reg.js
@@ -1,5 +1,4 @@
-document.querySelector('form').addEventListener('submit', auth);
-
-function auth() {
-  //
-}
+$(document).ready(function(){
+  $('#cpf').mask('000.000.000-00')
+  $('#tel').mask('(00) 00000-0000')
+});
